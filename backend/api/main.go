2aabfafedca9b45b3293afb5a4d2a2431c321e3d
Noah Hakansson <noah.hakansson@protonmail.com>
2022-11-03 08:29:10 +0100

    add development .env loading.

--- backend/api/main.go
+++ backend/api/main.go
@@ -2,10 +2,18 @@
 package main
 
 import (
+	"log"
+
 	"github.com/abodsakah/BTH-appen/backend/api/src/Routes"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	// load .env file into process for dev
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Println("DEV: Could not load .env file")
+	}
+
 	routes.SetupRoutes()
 }
