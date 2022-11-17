d2d4cacfa396b1d4c3fcfa81a476897c322a08dd
abodsakka <abodsakka2001@gmail.com>
2022-11-17 14:10:37 +0100

    fix: added scroll view and code cleanup

--- app/src/Views/Exams.js
+++ app/src/Views/Exams.js
@@ -1,87 +1,90 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import Container from '../Components/Container'
-import Button from '../Components/Button'
-import { Colors, Fonts } from '../style'
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Container from '../Components/Container';
+import Button from '../Components/Button';
+import { Colors, Fonts } from '../style';
+import Exam from '../Components/Exam';
 
 const Exams = () => {
-  return (
-    <Container>
-      <Text style={styles.code}>Exams</Text>
-      <Text style={styles.textStyle}>Registered exams</Text>
-      <Register />
-      <Register2 />
-      <Text style={styles.textStyle}>Comming exams</Text>
-      <Comming />
-    </Container>
-  )
-}
+	return (
+		<Container style={styles.container}>
+			<Text style={styles.title}>Exams</Text>
+			<ScrollView
+				style={styles.scrollContainer}
+				contentContainerStyle={styles.scrollContentStyle}
+			>
+				<Text style={styles.heading}>Registered exams</Text>
+				<Exam
+					code="PA1469"
+					name="Applikation Utveckling"
+					date="2023-05-31"
+					time="13:00"
+					room="J1240"
+					registered
+				/>
+				<Text style={styles.heading}>Comming exams</Text>
+				<Exam
+					code="DV1628"
+					name="Webbutveckling"
+					date="2023-05-31"
+					time="13:00"
+					room="J1240"
+				/>
+				<Exam
+					code="DV1628"
+					name="Webbutveckling"
+					date="2023-05-31"
+					time="13:00"
+					room="J1240"
+				/>
+				<Exam
+					code="DV1628"
+					name="Webbutveckling"
+					date="2023-05-31"
+					time="13:00"
+					room="J1240"
+				/>
+				<Exam
+					code="DV1628"
+					name="Webbutveckling"
+					date="2023-05-31"
+					time="13:00"
+					room="J1240"
+				/>
+				<Exam
+					code="DV1628"
+					name="Webbutveckling"
+					date="2023-05-31"
+					time="13:00"
+					room="J1240"
+				/>
+			</ScrollView>
+		</Container>
+	);
+};
 
-const Register = () => {
-    return (
-    <View style={styles.examContainer}>
-        <Text style={styles.code}>PA1469</Text>
-        <Text>Applikation Utveckling</Text>
-        <Text>Date: 2023-05-31</Text>
-        <Text>Time: 13:00</Text>
-        <Text>Room: J1240</Text>
-        <Button type="danger" style={styles.buttonStyle}>Unregister</Button>
-    </View>
-    )
-}
-
-const Register2 = () => {
-    return (
-    <View style={styles.examContainer}>
-        <Text style={styles.code}>MA1476</Text>
-        <Text>Matimatik introduktion</Text>
-        <Text>Date: 2023-05-31</Text>
-        <Text>Time: 13:00</Text>
-        <Text>Room: J1240</Text>
-        <Button type="danger" style={styles.buttonStyle}>Unregister</Button>
-    </View>
-    )
-}
-
-const Comming = () => {
-    return (
-    <View style={styles.examContainer}>
-        <Text style={styles.code}>
-            DV1628</Text>
-        <Text style={{flexDirection: 'row'}}>Datorteknik</Text>
-        <Text style={{flexDirection: 'row'}}>Date: 2023-05-31</Text>
-        <Text>Time: 13:00</Text>
-        <Text>Room: J1240</Text>
-        <Button style={styles.buttonStyle}>Register</Button>
-    </View>
-    )
-}
-
-export default Exams
+export default Exams;
 
 const styles = StyleSheet.create({
-    examContainer:{
-        padding: 10,
-        elevation: 10,
-        width: "100%",
-        backgroundColor: Colors.snowWhite,
-        justifyContent: 'flex-start',
-        borderRadius: 15,
-        margin:5
-    },
-    code:{
-        fontSize: Fonts.size.h3,
-        alignSelf: 'flex-start', 
-        fontFamily: Fonts.Inter_Bold,
-    },
-    buttonStyle:{
-        width: "50%",
-        alignSelf: "flex-end"
-    }, 
-    textStyle:{
-        fontSize: Fonts.size.h3,
-        borderRadius: 15,
-        margin:5,
-        alignSelf: 'flex-start', 
-    }
-})
+	container: {
+		paddingHorizontal: 0,
+	},
+	title: {
+		fontSize: Fonts.size.h1,
+		alignSelf: 'flex-start',
+		fontFamily: Fonts.Inter_Bold,
+		marginLeft: 10,
+	},
+	scrollContainer: {
+		width: '100%',
+	},
+	scrollContentStyle: {
+		paddingHorizontal: 15,
+		alignItems: 'center',
+	},
+	heading: {
+		fontSize: Fonts.size.h3,
+		margin: 5,
+		alignSelf: 'flex-start',
+	},
+});
