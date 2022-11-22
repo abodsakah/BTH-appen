c73f049cb11cace4d0b5288257607f26a8826234
Najem Hamo <najem.hamo@gmail.com>
2022-11-22 16:18:10 +0100

    scrapping news links

--- backend/api/src/Scraper/scraper.go
+++ backend/api/src/Scraper/scraper.go
@@ -3,12 +3,14 @@ package scraper
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"strings"
 
 	db "github.com/abodsakah/BTH-appen/backend/api/src/DB"
 	"github.com/gocolly/colly"
 )
 
+//GetNews function to get news from the website
 func GetNews() {
 	c := colly.NewCollector(
 		colly.AllowedDomains("www.bth.se"),
@@ -18,18 +20,21 @@ func GetNews() {
 
 	c.OnHTML(".Article-result", func(h *colly.HTMLElement) {
 
+		// Get the title
 		h.ForEach(".ArticleItem", func(_ int, e *colly.HTMLElement) {
 			title := e.ChildText("h2")
 			fmt.Println(title)
 			var date string
 			var des string
 
+			// Get the date
 			e.ForEach("p", func(_ int, e *colly.HTMLElement) {
 				if strings.Contains(e.Text, "Publicerad") {
 					date = e.Text
 					fmt.Println(date)
 				}
 			})
+			// Get the description
 			e.ForEach("div[class=article-category-page]", func(_ int, description *colly.HTMLElement) {
 				selction := description.DOM
 				childNodes := selction.Children().Nodes
@@ -39,54 +44,27 @@ func GetNews() {
 					des = selction.FindNodes(childNodes[0]).Text()
 				}
 			})
+			// Get the link
+			link := e.ChildAttr("a", "href")
+			fmt.Println(link)
 
 			article := db.News{
 				Title:       title,
 				Date:        date,
 				Description: des,
-				Link:        "Bth.se",
+				Link:        link,
 			}
 
 			newsList = append(newsList, article)
 		})
-
 	})
 
-	c.Visit("https://www.bth.se/category/nyheter")
+	err := c.Visit("https://www.bth.se/category/nyheter")
+	if err != nil {
+		log.Println(err)
+	}
+
 	results, _ := json.MarshalIndent(&newsList, "", "\n")
 
 	fmt.Printf("%s", results)
-
 }
-
-/*
-	// Get title
-	title := h.ChildText("h2")
-	newsList = append(newsList, db.News{Title: title})
-
-	// Get date
-	h.ForEach("p", func(_ int, d *colly.HTMLElement) {
-		if strings.Contains(d.Text, "Publicerad") {
-			date := d.Text
-			newsList = append(newsList, db.News{Date: date})
-		}
-	})
-
-	// Get description
-	h.ForEach("div[class=article-category-page]", func(_ int, description *colly.HTMLElement) {
-		selction := description.DOM
-		childNodes := selction.Children().Nodes
-		if len(childNodes) > 1 {
-			des := selction.FindNodes(childNodes[1]).Text()
-			newsList = append(newsList, db.News{Description: des})
-		} else if len(childNodes) == 1 {
-			des := selction.FindNodes(childNodes[0]).Text()
-			newsList = append(newsList, db.News{Description: des})
-		}
-	})
-
-	// Get link
-	h.ForEach()
-	link := h.Attr("href")
-	newsList = append(newsList, db.News{Link: link})
-*/
