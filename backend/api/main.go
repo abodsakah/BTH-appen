1cfc899a9e55630d1abd7fbbc16cda9b826e06f6
Noah Hakansson <noah.hakansson@protonmail.com>
2022-11-22 15:13:02 +0100

    Start on Expo push Notifications and refactor so `*gorm.DB` object is injected from main.go into all go routines.

--- backend/api/main.go
+++ backend/api/main.go
@@ -4,6 +4,8 @@ package main
 import (
 	"log"
 
+	"github.com/abodsakah/BTH-appen/backend/api/src/DB"
+	"github.com/abodsakah/BTH-appen/backend/api/src/Notifications"
 	"github.com/abodsakah/BTH-appen/backend/api/src/Routes"
 	"github.com/joho/godotenv"
 )
@@ -15,5 +17,18 @@ func main() {
 		log.Println("DEV: Could not load .env file")
 	}
 
-	routes.SetupRoutes()
+	gormDB, err := db.SetupDatabase()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// start notifications server go routine
+	go func() {
+		if err := notifications.StartServer(gormDB); err != nil {
+			log.Fatalln("Failed to start expo notifications server, error: ", err)
+		}
+	}()
+
+	// start API web server main routine
+	routes.SetupRoutes(gormDB)
 }
