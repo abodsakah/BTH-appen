a0592a05f03a8a2046f186aea3c6d9a842c5724e
DingusDungus <DingusDungus@github.com>
2022-11-29 12:54:34 +0100

    formatting done

--- backend/api/src/DB/database_test.go
+++ backend/api/src/DB/database_test.go
@@ -1,30 +1,32 @@
 package db
 
 import (
-  "testing"
+	"testing"
 
-  "github.com/stretchr/testify/assert"
-  "gorm.io/gorm"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
 
 	"github.com/joho/godotenv"
 )
 
-var db *gorm.DB
-var additionalTables = []string{"exam_users"}
+var (
+	db               *gorm.DB
+	additionalTables = []string{"exam_users"}
+)
 
 func TestDatabase(t *testing.T) {
-  err := godotenv.Load("../../../.env")
-  assert.Equal(t, nil, err, "Database can not be connected to")
+	err := godotenv.Load("../../../.env")
+	assert.Equal(t, nil, err, "Database can not be connected to")
 
-  db_p, err := SetupDatabase()
-  assert.NotEqual(t, nil, err, "Database can not be connected to")
-  db = db_p
+	db_p, err := SetupDatabase()
+	assert.NotEqual(t, nil, err, "Database can not be connected to")
+	db = db_p
 }
+
 func TestExample(t *testing.T) {
-  cleanUp(db, additionalTables)
-  assert.Equal(t, 1, 2, "They should be equal")
-  _, err := SetupDatabase()
-  assert.Equal(t, nil, err, "Database can not be connected to")
-  cleanUp(db, additionalTables)
+	cleanUp(db, additionalTables)
+	assert.Equal(t, 1, 2, "They should be equal")
+	_, err := SetupDatabase()
+	assert.Equal(t, nil, err, "Database can not be connected to")
+	cleanUp(db, additionalTables)
 }
-
