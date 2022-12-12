efdadcd4fc636a21aee43fa22489d4902098637d
Noah Hakansson <noah.hakansson@protonmail.com>
2022-12-12 12:24:03 +0100

    Add omitempty to json tags. removes empty field from generated JSON

--- backend/api/src/DB/models.go
+++ backend/api/src/DB/models.go
@@ -11,35 +11,35 @@ import (
 // User struct
 type User struct {
 	gorm.Model
-	Name     string  `form:"name" json:"name"`
-	Username string  `gorm:"uniqueIndex" form:"username" binding:"required" json:"username"`
-	Password string  `form:"password" binding:"required" json:"password"`
-	Role     string  `form:"role" gorm:"default:student" json:"role"`
-	Exams    []*Exam `gorm:"many2many:exam_users;" json:"exams"`
-	Tokens   []Token `json:"tokens"`
+	Name     string  `form:"name" json:"name,omitempty"`
+	Username string  `gorm:"uniqueIndex" form:"username" binding:"required" json:"username,omitempty"`
+	Password string  `form:"password" binding:"required" json:"password,omitempty"`
+	Role     string  `form:"role" gorm:"default:student" json:"role,omitempty"`
+	Exams    []*Exam `gorm:"many2many:exam_users;" json:"exams,omitempty"`
+	Tokens   []Token `json:"tokens,omitempty"`
 }
 
 // Exam struct
 type Exam struct {
 	gorm.Model
-	Name       string    `form:"name" binding:"required" json:"name"`
-	CourseCode string    `form:"course_code" binding:"required" json:"course_code"`
-	StartDate  time.Time `form:"start_date" binding:"required" json:"start_date"`
-	Users      []*User   `gorm:"many2many:exam_users;" json:"users"`
+	Name       string    `form:"name" binding:"required" json:"name,omitempty"`
+	CourseCode string    `form:"course_code" binding:"required" json:"course_code,omitempty"`
+	StartDate  time.Time `form:"start_date" binding:"required" json:"start_date,omitempty"`
+	Users      []*User   `gorm:"many2many:exam_users;" json:"users,omitempty"`
 }
 
 // News struct
 type News struct {
 	gorm.Model
-	Title       string    `gorm:"uniqueIndex" form:"title" binding:"required" json:"title"`
-	Date        time.Time `form:"date" binding:"required" json:"date"`
-	Description string    `form:"description" binding:"required" json:"description"`
-	Link        string    `form:"link" binding:"required" json:"link"`
+	Title       string    `gorm:"uniqueIndex" form:"title" binding:"required" json:"title,omitempty"`
+	Date        time.Time `form:"date" binding:"required" json:"date,omitempty"`
+	Description string    `form:"description" binding:"required" json:"description,omitempty"`
+	Link        string    `form:"link" binding:"required" json:"link,omitempty"`
 }
 
 // Token struct
 type Token struct {
 	gorm.Model
-	ExpoPushToken string `gorm:"uniqueIndex" form:"expo_push_token" binding:"required" json:"expo_push_token"`
-	UserID        uint   `json:"user_id"`
+	ExpoPushToken string `gorm:"uniqueIndex" form:"expo_push_token" binding:"required" json:"expo_push_token,omitempty"`
+	UserID        uint   `json:"user_id,omitempty"`
 }
