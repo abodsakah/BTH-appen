54c41cd984096af0dc4ec3bc113283e7053f6796
DingusDungus <DingusDungus@github.com>
2022-12-19 15:52:06 +0100

    Fixed changes to repo

--- backend/api/src/DB/exams_test.go
+++ backend/api/src/DB/exams_test.go
@@ -3,6 +3,7 @@ package db
 import (
 	"testing"
 
+	helpers "github.com/abodsakah/BTH-appen/backend/api/src/Helpers"
 	"github.com/joho/godotenv"
 	"github.com/stretchr/testify/assert"
 )
@@ -14,73 +15,73 @@ func TestDatabaseExam(t *testing.T) {
 	}
 	dbP, err := SetupDatabase()
 	assert.Nil(t, err, "Database can not be connected to")
-	db = dbP
+	helpers.DbGorm = dbP
 }
 
 func TestCreateExam1(t *testing.T) {
-	_, err := fixtureWrap(t, &testExam)
+	_, err := helpers.FixtureWrapCreate(t, &helpers.TestExam)
 	assert.Nil(t, err, "After fixture is run a create will be called that shall return no error")
 }
 
 func TestCreateExam2(t *testing.T) {
-	_, _ = fixtureWrap(t, &testExam)
-	err := CreateExam(db, testExam)
+	_, _ = helpers.FixtureWrapCreate(t, &helpers.TestExam)
+	err := CreateExam(helpers.DbGorm, helpers.TestExam)
 	assert.NotNil(t, err, "Trying to create a duplicate exam shall return an error")
 }
 
 func TestDeleteExam1(t *testing.T) {
-	id, _ := fixtureWrap(t, &testExam)
-	_, err := DeleteExam(db, id)
+	id, _ := helpers.FixtureWrapCreate(t, &helpers.TestExam)
+	_, err := DeleteExam(helpers.DbGorm, id)
 	assert.Nil(t, err, "When deleting the object it shall not return an error")
 }
 
 func TestAddUserToExam1(t *testing.T) {
-	idExam, _ := fixtureWrap(t, &testExam)
-	idUser, _ := createUserWrap()
-	_, err := AddUserToExam(db, idExam, idUser)
+	idExam, _ := helpers.FixtureWrapCreate(t, &helpers.TestExam)
+	helpers.DbGorm.Create(&helpers.TestUser)
+	_, err := AddUserToExam(helpers.DbGorm, idExam, helpers.TestUser.ID)
 	assert.Nil(t, err, "Adding an existing user to an existing exam, with no duplicates, should create no errors")
 }
 
 func TestAddUserToExam2(t *testing.T) {
-	idExam, _ := fixtureWrap(t, &testExam)
-	idUser, _ := createUserWrap()
-	_, _ = AddUserToExam(db, idExam, idUser)
-	_, err := AddUserToExam(db, idExam, idUser)
+	idExam, _ := helpers.FixtureWrapCreate(t, &helpers.TestExam)
+	helpers.DbGorm.Create(&helpers.TestUser)
+	_, _ = AddUserToExam(helpers.DbGorm, idExam, helpers.TestUser.ID)
+	_, err := AddUserToExam(helpers.DbGorm, idExam, helpers.TestExam.ID)
 	assert.NotNil(t, err, "Trying to add a duplicate entry should return an error")
 }
 
 func TestRemoveUserFromExam1(t *testing.T) {
-	idExam, _ := fixtureWrap(t, &testExam)
-	idUser, _ := createUserWrap()
-	_, _ = AddUserToExam(db, idExam, idUser)
-	_, err := RemoveUserFromExam(db, idExam, idUser)
+	idExam, _ := helpers.FixtureWrapCreate(t, &helpers.TestExam)
+	helpers.DbGorm.Create(&helpers.TestUser)
+	_, _ = AddUserToExam(helpers.DbGorm, idExam, helpers.TestUser.ID)
+	_, err := RemoveUserFromExam(helpers.DbGorm, idExam, helpers.TestExam.ID)
 	assert.Nil(t, err, "Removing an existing entry shall not return any errors")
 }
 
 func TestRemoveUserFromExam2(t *testing.T) {
-	idExam, _ := fixtureWrap(t, &testExam)
-	idUser, _ := createUserWrap()
-	_, err := RemoveUserFromExam(db, idExam, idUser)
+	idExam, _ := helpers.FixtureWrapCreate(t, &helpers.TestExam)
+	helpers.DbGorm.Create(&helpers.TestUser)
+	_, err := RemoveUserFromExam(helpers.DbGorm, idExam, helpers.TestUser.ID)
 	assert.NotNil(t, err, "Removing a non-existent entry shall return an error")
 }
 
 func TestGetExamsDueSoon(t *testing.T) {
-	_, _ = fixtureWrap(t, &testExam)
-	exams, _ := GetExamsDueSoon(db)
+	_, _ = helpers.FixtureWrapCreate(t, &helpers.TestExam)
+	exams, _ := GetExamsDueSoon(helpers.DbGorm)
 	assert.Less(t, 0, len(exams), "After an exam has been created with the current date plus one day, it should come up in the array of due exams")
 }
 
 func TestGetExamUsers1(t *testing.T) {
-	idExam, _ := fixtureWrap(t, &testExam)
-	idUser, _ := createUserWrap()
-	_, _ = AddUserToExam(db, idExam, idUser)
-	users, _ := GetExamUsers(db, idExam)
+	idExam, _ := helpers.FixtureWrapCreate(t, &helpers.TestExam)
+	helpers.DbGorm.Create(&helpers.TestUser)
+	_, _ = AddUserToExam(helpers.DbGorm, idExam, helpers.TestUser.ID)
+	users, _ := GetExamUsers(helpers.DbGorm, idExam)
 	assert.Less(t, 0, len(users), "GetExamUsers should return 1 entry in the array after a user has been added to the exam")
 }
 
 func TestGetExamUsers2(t *testing.T) {
-	idExam, _ := fixtureWrap(t, &testExam)
-	_, _ = createUserWrap()
-	users, _ := GetExamUsers(db, idExam)
+	idExam, _ := helpers.FixtureWrapCreate(t, &helpers.TestExam)
+	helpers.DbGorm.Create(&helpers.TestUser)
+	users, _ := GetExamUsers(helpers.DbGorm, idExam)
 	assert.Equal(t, 0, len(users), "GetExamUsers should return 0 entries in the array when no user has been added to exam")
 }
