282f24608d159e4b135dd0bafef53547aa9227e0
DingusDungus <DingusDungus@github.com>
2022-12-19 15:43:19 +0100

    Fixed linting and formatting issues

--- backend/api/src/Routes/routes_test.go
+++ backend/api/src/Routes/routes_test.go
@@ -2,6 +2,11 @@
 package routes
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 	"time"
 
@@ -12,24 +17,15 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"net/http"
-	"net/http/httptest"
-
-	"bytes"
-	"encoding/json"
-	"io"
-
 	"github.com/gin-gonic/gin"
 )
 
-var (
-  router *gin.Engine
-)
+var router *gin.Engine
 
 func startTest(t *testing.T) {
-  helpers.FixtureWrapCreate(t, helpers.TestExam, helpers.TestNews)
-  temp := *helpers.TestUser
-  db.CreateUser(helpers.DbGorm, &temp)
+	_, _ = helpers.FixtureWrapCreate(t, helpers.TestExam, helpers.TestNews)
+	temp := *helpers.TestUser
+	_ = db.CreateUser(helpers.DbGorm, &temp)
 }
 
 func setupContext() (*gin.Context, *httptest.ResponseRecorder) {
@@ -70,75 +66,74 @@ func TestDatabaseRoutes(t *testing.T) {
 }
 
 func TestSetupRoutes(t *testing.T) {
-  var err error
-  router, err = SetupRoutes(helpers.DbGorm)
-  assert.Nil(t, err, "Trying to set up routes shall not cause any errors")
+	var err error
+	router, err = SetupRoutes(helpers.DbGorm)
+	assert.Nil(t, err, "Trying to set up routes shall not cause any errors")
 }
 
 func TestLogin1(t *testing.T) {
-   startTest(t)
-   c, w := setupContext()
- 	// Set Body, Header and Content-Type
- 	mockJSONPost(c, &gin.H{
- 		"Username": "test",
-     "Password": "pass",
- 	})
-
- 	// call API endpoint
- 	login(c)
-   assert.Equal(t, 200, w.Code, "When trying to log in with correct admin credentials it shall return status: 200")
-}
-
-func TestLogin2(t *testing.T) {
-  startTest(t)
-  c, w := setupContext()
+	startTest(t)
+	c, w := setupContext()
 	// Set Body, Header and Content-Type
 	mockJSONPost(c, &gin.H{
-		"Username": "admin",
-    "Password": "incorrect-password",
+		"Username": "test",
+		"Password": "pass",
 	})
 
 	// call API endpoint
 	login(c)
-  assert.NotEqual(t, 200, w.Code, "When trying to log in with incorrect admin credentials it shall not return status: 200")
+	assert.Equal(t, 200, w.Code, "When trying to log in with correct admin credentials it shall return status: 200")
 }
 
-
-func TestExamCreate1(t *testing.T) {
-  helpers.FixtureWrapNonCreate(t)
-  c, w := setupContext()
+func TestLogin2(t *testing.T) {
+	startTest(t)
+	c, w := setupContext()
 	// Set Body, Header and Content-Type
 	mockJSONPost(c, &gin.H{
-		"name": "Test exam",
-    "course_code": "something",
-    "room": "something",
-    "start_date": time.Now(),
+		"Username": "admin",
+		"Password": "incorrect-password",
+	})
+
+	// call API endpoint
+	login(c)
+	assert.NotEqual(t, 200, w.Code, "When trying to log in with incorrect admin credentials it shall not return status: 200")
+}
+
+func TestExamCreate1(t *testing.T) {
+	_ = helpers.FixtureWrapNonCreate(t)
+	c, w := setupContext()
+	// Set Body, Header and Content-Type
+	mockJSONPost(c, &gin.H{
+		"name":        "Test exam",
+		"course_code": "something",
+		"room":        "something",
+		"start_date":  time.Now(),
 	})
 
 	// call API endpoint
 	createExam(c)
-  assert.Equal(t, 200, w.Code, "When trying to call on create Exam API with no duplicates present, it should return status: 200")
+	assert.Equal(t, 200, w.Code, "When trying to call on create Exam API with no duplicates present, it should return status: 200")
 }
 
 func TestExamCreate2(t *testing.T) {
-  startTest(t)
-  c, w := setupContext()
+	startTest(t)
+	c, w := setupContext()
 	// Set Body, Header and Content-Type
 	mockJSONPost(c, &gin.H{
-		"name": "Test exam",
-    "course_code": "something",
-    "room": "something",
-    "start_date": time.Now(),
+		"name":        "Test exam",
+		"course_code": "something",
+		"room":        "something",
+		"start_date":  time.Now(),
 	})
 
 	// call API endpoint
 	createExam(c)
-  assert.NotEqual(t, 200, w.Code, "When trying to call on create Exam API with duplicates present, it should not return status: 200")
+	assert.NotEqual(t, 200, w.Code, "When trying to call on create Exam API with duplicates present, it should not return status: 200")
 }
 
 func TestExamDelete1(t *testing.T) {
-  startTest(t)
-  c, w := setupContext()
+	startTest(t)
+	c, w := setupContext()
 	// Set Body, Header and Content-Type
 	mockJSONPost(c, &gin.H{
 		"exam_id": 1,
@@ -146,12 +141,12 @@ func TestExamDelete1(t *testing.T) {
 
 	// call API endpoint
 	deleteExam(c)
-  assert.Equal(t, 200, w.Code, "When trying to call on delete Exam API with entry present, it should return status: 200")
+	assert.Equal(t, 200, w.Code, "When trying to call on delete Exam API with entry present, it should return status: 200")
 }
 
 func TestExamDelete2(t *testing.T) {
-  helpers.FixtureWrapNonCreate(t)
-  c, w := setupContext()
+	_ = helpers.FixtureWrapNonCreate(t)
+	c, w := setupContext()
 	// Set Body, Header and Content-Type
 	mockJSONPost(c, &gin.H{
 		"exam_id": 1,
@@ -159,12 +154,12 @@ func TestExamDelete2(t *testing.T) {
 
 	// call API endpoint
 	deleteExam(c)
-  assert.NotEqual(t, 200, w.Code, "When trying to call on delete Exam API with no entry present, it should not return status: 200")
+	assert.NotEqual(t, 200, w.Code, "When trying to call on delete Exam API with no entry present, it should not return status: 200")
 }
 
 func TestListExams1(t *testing.T) {
-  startTest(t)
-  c, w := setupContext()
+	startTest(t)
+	c, w := setupContext()
 	// Set Body, Header and Content-Type
 	mockJSONPost(c, &gin.H{
 		"exam_id": 1,
@@ -172,12 +167,12 @@ func TestListExams1(t *testing.T) {
 
 	// call API endpoint
 	listExams(c)
-  assert.Equal(t, 200, w.Code, "When trying to call on list Exams API with entries present, it should return status: 200")
+	assert.Equal(t, 200, w.Code, "When trying to call on list Exams API with entries present, it should return status: 200")
 }
 
 func TestListDueExams1(t *testing.T) {
-  startTest(t)
-  c, w := setupContext()
+	startTest(t)
+	c, w := setupContext()
 	// Set Body, Header and Content-Type
 	mockJSONPost(c, &gin.H{
 		"exam_id": 1,
@@ -185,36 +180,34 @@ func TestListDueExams1(t *testing.T) {
 
 	// call API endpoint
 	listDueExams(c)
-  assert.Equal(t, 200, w.Code, "When trying to call on list due Exams API with due exams present, it should return status: 200")
+	assert.Equal(t, 200, w.Code, "When trying to call on list due Exams API with due exams present, it should return status: 200")
 }
 
 func TestListUserExams(t *testing.T) {
-  startTest(t)
-  db.AddUserToExam(helpers.DbGorm, 1, 1)
-  c, w := setupContext()
+	startTest(t)
+	_, _ = db.AddUserToExam(helpers.DbGorm, 1, 1)
+	c, w := setupContext()
 	// Set Body, Header and Content-Type
 	mockJSONPost(c, &gin.H{
-    "userID": 1,
+		"userID":  1,
 		"exam_id": 1,
 	})
-  c.Set("UserID", uint(1))
+	c.Set("UserID", uint(1))
 	// call API endpoint
 	listUserExams(c)
-  assert.Equal(t, 200, w.Code, "When trying to call on list User Exams API with exams present for user, it should return status: 200")
+	assert.Equal(t, 200, w.Code, "When trying to call on list User Exams API with exams present for user, it should return status: 200")
 }
 
 func TestListExamUser(t *testing.T) {
-  startTest(t)
-  db.AddUserToExam(helpers.DbGorm, 1, 1)
-  c, w := setupContext()
+	startTest(t)
+	_, _ = db.AddUserToExam(helpers.DbGorm, 1, 1)
+	c, w := setupContext()
 	// Set Body, Header and Content-Type
 	mockJSONPost(c, &gin.H{
 		"exam_id": 1,
 	})
-  c.Set("UserID", uint(1))
+	c.Set("UserID", uint(1))
 	// call API endpoint
 	listExamUsers(c)
-  assert.Equal(t, 200, w.Code, "When trying to call on list Exam Users API with users present for user, it should return status: 200")
+	assert.Equal(t, 200, w.Code, "When trying to call on list Exam Users API with users present for user, it should return status: 200")
 }
-
-
