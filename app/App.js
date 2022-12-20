fb30ab7bd06168151c91ec233aa275f2a65d94d7
abodsakka <abodsakka2001@gmail.com>
2022-12-20 13:32:47 +0100

    fix: no push token for unlogged in users - The application is not going to generate a token and handle notifications for users that not loged in

--- app/App.js
+++ app/App.js
@@ -95,31 +95,35 @@ export default function App() {
 
 	useEffect(() => {
 		getPreferredLanguageAndApply();
-
-		// Expo push notifications
-		registerForPushNotificationsAsync().then((token) =>
-			setExpoPushToken(token)
-		);
-
-		// This listener is fired whenever a notification is received while the app is foregrounded
-		notificationListener.current =
-			Notifications.addNotificationReceivedListener((notification) => {
-				setNotification(notification);
-			});
-
-		// This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
-		responseListener.current =
-			Notifications.addNotificationResponseReceivedListener((response) => {
-				console.log(response);
-			});
-
-		getUserFromSecureStorage();
-
-		return () => {
-			Notifications.removeNotificationSubscription(notificationListener);
-			Notifications.removeNotificationSubscription(responseListener);
-		};
 	}, []);
+
+	useEffect(() => {
+		if (user) {
+			// Expo push notifications
+			registerForPushNotificationsAsync().then((token) =>
+				setExpoPushToken(token)
+			);
+
+			// This listener is fired whenever a notification is received while the app is foregrounded
+			notificationListener.current =
+				Notifications.addNotificationReceivedListener((notification) => {
+					setNotification(notification);
+				});
+
+			// This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
+			responseListener.current =
+				Notifications.addNotificationResponseReceivedListener((response) => {
+					console.log(response);
+				});
+
+			getUserFromSecureStorage();
+
+			return () => {
+				Notifications.removeNotificationSubscription(notificationListener);
+				Notifications.removeNotificationSubscription(responseListener);
+			};
+		}
+	}, [user]);
 
 	if (!fontsLoaded) {
 		return <ActivityIndicator />;
