dbf2c0f7f54d5717ba98f30a6798a7be6f62725f
abodsakka <abodsakka2001@gmail.com>
2022-12-26 15:02:19 +0100

    Finished news section

--- app/src/Components/NewsCard.js
+++ app/src/Components/NewsCard.js
@@ -1,56 +1,62 @@
 import { View, StyleSheet, Text, SafeAreaView, Image} from "react-native";
-import React, { startTransition } from "react";
+import React from 'react';
+import { Colors, Fonts } from '../style';
+import { TouchableRipple } from 'react-native-paper';
 
-const NewsCard = () => {
-    return (
-        <SafeAreaView style={styles.container}>
-          <View style={{padding: 20}}>  
-          {/* Title */}
-            <Text style={styles.title}> Text from the Database </Text>
+const NewsCard = ({ style, title, description, date, onPress = () => {} }) => {
+	return (
+		<View style={[styles.container, style]}>
+			<TouchableRipple onPress={onPress} style={styles.touchableRipple}>
+				<View style={{ padding: 20 }}>
+					{/* Title */}
+					<Text style={styles.title}>{title}</Text>
 
-          {/* Description */}
-            <Text style={styles.description}> This is the description </Text>
+					{/* Description */}
+					<Text style={styles.description}>{description}</Text>
 
-            <View style={styles.data}>
-              <Text style={styles.date}> 2021-05-05 </Text>
-            </View> 
-            </View>
-
-        </SafeAreaView>
-
-    );
-}
+					<View style={styles.data}>
+						<Text style={styles.date}>Published: {date}</Text>
+					</View>
+				</View>
+			</TouchableRipple>
+		</View>
+	);
+};
 
 export default NewsCard;
 
 const styles = StyleSheet.create({
-    container: {
-      width : '98%',
-      alignSelf : 'center',
-      borderRadius : 20,
-      shadowOpacity : 0.5,
-      shadowColor : '#000',
-      shadowOffset : {width : 5, height : 5},
-      backgroundColor : '#fff',
-      marginTop : 20
-    },
-    title : {
-      fontSize : 18,
-      fontWeight : '600',
-      merginTop : 10
-    },
-    description : {
-      fontSize : 16,
-      fontWeight : '400',
-      merginTop : 10
-    },
-    data: {
-      flexDirection : 'row',
-      justifyContent : 'space-between',
-      merginTop : 10 
-    },
-    date : {
-      fontWeight : 'bold'
-    }
-
-  })
+	container: {
+		width: '100%',
+		alignSelf: 'center',
+		borderRadius: 20,
+		shadowOpacity: 0.5,
+		shadowColor: '#000',
+		shadowOffset: { width: 5, height: 5 },
+		backgroundColor: Colors.snowWhite,
+		marginTop: 20,
+		elevation: 5,
+	},
+	touchableRipple: {
+		width: '100%',
+		borderRadius: 20,
+	},
+	title: {
+		fontSize: Fonts.size.h3,
+		fontFamily: Fonts.Inter_Bold,
+		merginTop: 10,
+	},
+	description: {
+		fontSize: Fonts.size.h6,
+		fontFamily: Fonts.Inter_Medium,
+		merginTop: 10,
+	},
+	data: {
+		flexDirection: 'row',
+		justifyContent: 'flex-end',
+		paddingTop: 10,
+	},
+	date: {
+		fontFamily: Fonts.Inter_Light,
+	},
+});
